643de2639a7ab2cd15bd845719bf81242a996c1e
Amritendu Mohan Roy <amritenduroy70@gmail.com>
2022-07-12 18:28:00 +0530

    fetch bug

--- embibe/src/components/GivePaper.jsx
+++ embibe/src/components/GivePaper.jsx
@@ -14,29 +14,78 @@ const GivePaper = () => {
 	// }
 
 	const [count, setCount] = useState(0);
-	useEffect(() => {
-		axios.get("http://localhost:8000/api/student/givepaper").then((res) => {
+	let itemList = [];
+
+	const [result, setResult] = useState([]);
+	useEffect(()=> {
+		getData()
+		getCount()
+
+	}, [])
+	
+
+
+	const getCount = async() => {
+	 axios.get("http://localhost:8000/api/student/givepaper").then((res) => {
 			console.log(res.data);
 			setCount(res.data);
-
+			console.log("count", count)
 
 		});
-		axios.get("http://localhost:8000/api/student/getpapers").then((res) => {
-			setComponents(res.data.papers);
-		})
-	}, []);
 	
-	let itemList = [];
-	for (let i = 0; i < count; i++) {
-		const name = components[i].name;
-		const time = components[i].time;
-		const subject = components[i].subject;
-		console.log(name)
-		itemList.push(<QuestionCards name={name} time={time} sub={subject} />)
+		
+	}
+	const getData = async() => {
+		// 	 await axios.get("http://localhost:8000/api/student/getpapers").then((res) => {
+		// 		result = res.data;
+		// 		setComponents(result);
+		// 		console.log("result",result)
+		// })
+
+		// return result;
+		// .then((res) => {
+		// 	setComponents(res.data.papers);	
+		// console.log("res", res)
+			
+		// })
+		// console.log("status", data)
+		// return data;
+
+
+		await fetch("http://localhost:8000/api/student/getpapers").then((res) => res.json()).then((data) => {
+			
+			console.log("data", data)
+			
+			setResult( data.papers);
+			console.log("result->",result)
+		
+
+		})
+		
 	}
 
+	// console.log("data1", result);
+	// setComponents(result);
+	// console.log("components", components);
+	// console.log("count", count)
+	// console.log("data", data)
+	// console.log("components", components)
+	
+// setTimeout(() => {
+// 	console.log(result)
+// }, 4000);
+for (let i = 0; i < count; i++) {
+	const name = result[i].name;
+	const time = result[i].time;
+	const subject = result[i].subject;
+	// const marks = result[count-i-1].marks;
+	// const nq = result[count-i-1].nq;
 
-
+	// console.log(name)
+	itemList.push(<QuestionCards name={name} time={time} sub={subject}/>)
+}
+console.log("itemlist", itemList)
+// console.log(result)
 	return (
 		<Div d="flex" bg="#f7fafa">
 			<SidebarStudent />
