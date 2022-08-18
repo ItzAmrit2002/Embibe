1792e8a3efa010125c842b8db53a78575b80ebab
Cowwl <84736016+Cowwl@users.noreply.github.com>
2022-08-19 00:33:43 +0530

    Updated results

---
+++ embibe/src/components/PieCharts.jsx
@@ -0,0 +1,57 @@
+import { Div, Button, Text, Icon, Container, Label, Checkbox } from "atomize";
+import { useState } from "react";
+import axios from "axios";
+import { Chart } from 'react-charts'
+import { PieChart } from 'react-minimal-pie-chart';
+import "./PieCharts.css";
+const PieCharts = () => {
+    return (
+        <Div
+            d="flex"
+            bg="#f4decb"
+            h="30vh"
+            w="25vw"
+            align="center"
+            justify="center"
+            m="4rem"
+            shadow="3"
+            border="1px solid"
+            borderColor="#94618e"
+            rounded="lg"
+            flexDir="column"
+        >
+            <Div bg="#FF8FB1" w="100%" textAlign="center" rounded="lg" shadow="3">
+                <Text
+                    fontFamily="Itim"
+                    textSize="display3"
+                    textColor="#f8eee7"
+                >
+                    Marks
+                </Text>
+            </Div>
+            <Div d="flex" maxW="20vw" justify="space-between" align="center">
+                <PieChart
+                    data={[
+                        { title: 'One', value: 360, color: '#f8eee7' },
+                        { title: 'Two', value: 200, color: '#B270A2' },
+                    ]}
+                    animate={true}
+                    animationDuration={500}
+                    radius={40}
+                />
+                <Text
+                    fontFamily="Itim"
+                    textSize="display1"
+                    textColor="#49274a"
+                    textWeight="100"
+                    className="text-marks"
+                    m={{ x: "1rem" }}
+                >
+                    200/360
+                </Text>
+            </Div>
+        </Div>
+    );
+}
+
+export default PieCharts;
--- embibe/src/components/Results.jsx
+++ embibe/src/components/Results.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Div, Button, Text, Icon, Container, Label, Checkbox } from "atomize";
+import { useState } from "react";
+import axios from "axios";
+import { Chart } from 'react-charts'
+import PieCharts from './PieCharts';
 
 const Results = () => {
   return (
-    <div>Results</div>
+    <Div d="flex" bg="#FCE2DB" flexDir="column" h="100vh">
+      <Div d="flex" bg="#FF8FB1" h="10vh" w="100%" align="center" justify="center" textAlign="center" shadow="3">
+        <Text
+          fontFamily="Cedarville Cursive"
+          textSize="display3"
+          textWeight="800"
+          textColor="#121212">
+          Embibe
+        </Text>
+      </Div>
+      <PieCharts/>
+    </Div>
   )
 }
 
