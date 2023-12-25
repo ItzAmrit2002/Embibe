c0fc95daccead055c28a23840469ad346cab3246
Cowwl <84736016+Cowwl@users.noreply.github.com>
2023-12-25 21:02:33 +0530

    Update Results.jsx

--- embibe/src/components/Results.jsx
+++ embibe/src/components/Results.jsx
@@ -1,72 +1,123 @@
-import React, { useEffect } from 'react'
-import { Div, Button, Text, Icon, Container, Label, Checkbox, Row, Col } from "atomize";
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
+import useAuth from "../hooks/useAuth";
+import { Container, Div, Text, Row, Col } from "atomize";
 import axios from "axios";
-import { Chart } from 'react-charts'
-import PieCharts from './PieCharts';
-import { useParams } from 'react-router-dom';
+import PieCharts from "./PieCharts";
+import { useParams } from "react-router-dom";
 
 const Results = () => {
   const { pid, sid, totalmarks } = useParams();
-  const [marks, setMarks] = useState(0);
+  const { auth } = useAuth();
+  const [papers, setPapers] = useState([]);
+
+  const getMarks = async () => {
+    try {
+      const res = await axios.post(
+        "https://testhubbknd.onrender.com/api/student/getmarks",
+        {
+          userid: auth.id,
+        }
+      );
+      console.log("Marks res= ", res);
+      setPapers(res.data);
+    } catch (err) {
+      console.error("Error fetching marks:", err);
+    }
+  };
 
   useEffect(() => {
-    getData()
-
+    getMarks();
   }, []);
 
-  const getData = async () => {
-    await axios
-      .post("https://testhubbknd.onrender.com/api/tally/getmarks", {
-        paper_id: pid,
-        user_id: sid,
-
-      })
-      .then((res) => {
-        console.log(res);
-        setMarks(res.data.marks);
-
-
-      }).catch((err) => console.log(err));
-
-
-  }
   return (
-    <Div d="flex" bg="#FCE2DB" flexDir="column" h="100%">
-      <Div d="flex" bg="#94618e" h="10vh" w="100%" align="center" justify="center" textAlign="center" shadow="3" p="2%">
+    <Div d="flex" bg="#FCE2DB" flexDir="column" h="100%" overflow="visible">
+      <Div
+        d="flex"
+        bg="#94618e"
+        h="100%"
+        w="100%"
+        align="center"
+        justify="center"
+        textAlign="center"
+        shadow="3"
+        p="1rem"
+      >
         <Text
           fontFamily="Cedarville Cursive"
           textSize="display3"
           textWeight="800"
-          textColor="#f8eee7">
+          textColor="#f8eee7"
+        >
           Embibe
         </Text>
       </Div>
-     
-      <Row w="100%" h="100%">
-        <Col h="100%" w="100%" >
-          <PieCharts marks={marks} totalmarks={totalmarks} heading={"Marks"}/>
-          </Col>
-          <Col h="100%" w="100%" >
-          <PieCharts marks={marks} totalmarks={totalmarks} heading={"Questions Answered"}/>
-          </Col>
-          <Col h="100%" w="100%">
-          <PieCharts marks={marks} totalmarks={totalmarks} heading={"Marks"}/>
-          </Col>
-          <Col h="100%" w="100%">
-          <PieCharts marks={marks} totalmarks={totalmarks} heading={"Marks"}/>
-          </Col>
-          <Col h="100%" w="100%">
-          <PieCharts marks={marks} totalmarks={totalmarks} heading={"Marks"}/>
-          </Col>
-          <Col h="100%" w="100%">
-          <PieCharts marks={marks} totalmarks={totalmarks} heading={"Marks"}/>
-          </Col>
-          
-      </Row>
-      </Div>
-    
-  )
-}
 
-export default Results
+      <Div
+        d="flex"
+        flexDir="row"
+        p="2%"
+        minH="100vh"
+        h="auto"
+        align="center"
+        justify="center"
+      >
+        {papers.map((item, index) => (
+          <Div
+            key={index}
+            w="100%"
+            align="center"
+            justify="center"
+            flexDir="column"
+          >
+            <Text
+              textSize="display1"
+              textWeight="500"
+              textColor="#333"
+              textAlign="center"
+            >
+              Paper {index + 1} Results
+            </Text>
+
+            {/* Display each chart in a 2x2 grid */}
+            <Container className="center" d="flex" flexDir="column">
+              <Row>
+                <Col size="6">
+                  <PieCharts
+                    marks={item.marks}
+                    totalmarks={totalmarks}
+                    heading={`Marks`}
+                  />
+                </Col>
+                <Col size="6">
+                  <PieCharts
+                    marks={item.attempted}
+                    totalmarks={totalmarks}
+                    heading={`Attempted`}
+                  />
+                </Col>
+              </Row>
+              <Row>
+                <Col size="6">
+                  <PieCharts
+                    marks={item.correct}
+                    totalmarks={totalmarks}
+                    heading={`Correct`}
+                  />
+                </Col>
+                <Col size="6">
+                  <PieCharts
+                    marks={item.incorrect}
+                    totalmarks={totalmarks}
+                    heading={`Incorrect`}
+                  />
+                </Col>
+              </Row>
+            </Container>
+          </Div>
+        ))}
+      </Div>
+    </Div>
+  );
+};
+
+export default Results;
